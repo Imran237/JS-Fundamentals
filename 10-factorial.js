f2cec8bc3cde9f9bf43a464cd9da7f587db8574d
Imrane Jinadu <imranjinadu57@gmail.com>
2025-07-10 09:00:24 +0000

    Updated 10-factorial.js to use function keyword

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,8 +1,16 @@
 const args = process.argv.slice(2);
 
-const n = isNaN(Number(args[0])) ? 1 : Number(args[0]);
+// Define the factorial function using the `function` keyword
+function factorial(x) {
+  if (x <= 1) {
+    return 1;
+  }
+  return x * factorial(x - 1);
+}
 
-const factorial = (x) =>
-  x <= 1 ? 1 : x * factorial(x - 1);
+// Get and validate input
+const input = args[0];
+const n = isNaN(input) ? 1 : Math.floor(Number(input));
 
+// Print result
 console.log(factorial(n));
