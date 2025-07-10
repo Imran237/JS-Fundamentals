b67ad468a7117b56620327d75932cf675d52ab7f
Imrane Jinadu <imranjinadu57@gmail.com>
2025-07-10 08:13:46 +0000

    Add Arguments

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+const args = process.argv.slice(2);
+
+const first = args[0] !== undefined ? args[0] : 'undefined';
+const second = args[1] !== undefined ? args[1] : 'undefined';
+
+console.log(`${first} is ${second}`);
